b95e46e3b0415e298b475861867aa7eca4df9737
Lance James <lancejames@unit221b.com>
2021-09-02 17:02:24 -0400

    urgh

--- messaging.py
+++ messaging.py
@@ -122,10 +122,11 @@ def getkeys(user):
     
 def read_email_from_gmail(window,messages = data, downloadkeys = False, SMTP_SERVER="imap.gmail.com", SMTP_PORT=993):
     #global data
-    if len(messages) == 1: 
-        messages.pop()
-    else:
-        messages = []
+    
+
+
+
+
     Q = False
     waitmessages = []
     userpubkeys = dict()
