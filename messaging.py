34e4bba8a168067d3a6e7ce86bc66a0f746a0764
Nick Pence <pencenj@gmail.com>
2021-09-17 17:37:48 -0400

    fix timezones

--- messaging.py
+++ messaging.py
@@ -139,6 +139,9 @@ def convert_datetime(email_date):
         datetimeobj = arrow.get(email_date, 'DD MMM YYYY HH:mm:ss ZZZ')
     else:
         datetimeobj = arrow.get(email_date, 'DD MMM YYYY HH:mm:ss Z')
+    timezone = datetimeobj.format('Z')[:3]
+    datetimeobj.shift(hours=int(timezone))
+    datetimeobj.replace(tzinfo='+00:00')
     return datetimeobj
     
 def read_email_from_gmail(window,messages = data, downloadkeys = False, SMTP_SERVER="imap.gmail.com", SMTP_PORT=993):
@@ -237,6 +240,9 @@ def read_email_from_gmail(window,messages = data, downloadkeys = False, SMTP_SER
                                 for i in range(len(messages)):
                                     if type(messages[i][1]) == type(''):
                                         messages[i][1] = arrow.get(messages[i][1], 'ddd, D MMM YYYY h:mm:ss A')
+                                        timezone = messages[i][1].format('Z')[:3]
+                                        messages[i][1].shift(hours=int(timezone))
+                                        messages[i][1].replace(tzinfo='+00:00')
                                 messages.append([email_from, convert_datetime(msg['Date']), userpubkeys[email_from],plaintext['plaintext']])
                                 messages.sort(key=lambda x: x[1])
                                 newmessages = parse_datetime(messages)
